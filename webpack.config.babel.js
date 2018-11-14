290d95763a02435207739de7da5e3d94c9a2fd7e
LesterGallagher <LesterGallagher@gmail.com>
2018-11-14 13:40:22 +0100

    babel

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,12 +3,23 @@ const path = require('path');
 module.exports = {
     context: __dirname,
     entry: path.resolve(__dirname, 'src/index.js'),
-    mode: 'development',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname),
         filename: 'html2js.js',
     },
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            }
+        ]
     }
 }
